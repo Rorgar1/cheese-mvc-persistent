735954322cd2d5e4cf94385790050fbf272212b1
Robin Bailey <rorgar1@yahoo.com>
2019-09-07 12:27:18 -0500

    Commit after lesson 12

--- src/main/java/org/launchcode/models/Cheese.java
+++ src/main/java/org/launchcode/models/Cheese.java
@@ -1,10 +1,9 @@
 package org.launchcode.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 /**
  * Created by LaunchCode
@@ -24,7 +23,10 @@ public class Cheese {
     @Size(min=1, message = "Description must not be empty")
     private String description;
 
-    private CheeseType type;
+    @ManyToOne
+    private Category category;
+
+   /* private CheeseType type; */
 
     public Cheese(String name, String description) {
         this.name = name;
@@ -52,12 +54,20 @@ public class Cheese {
     public void setDescription(String description) {
         this.description = description;
     }
+    public Category getCategory() {
+        return category;
+    }
 
-    public CheeseType getType() {
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+   /* public CheeseType getType() {
         return type;
     }
 
     public void setType(CheeseType type) {
         this.type = type;
-    }
+    } */
+
 }
